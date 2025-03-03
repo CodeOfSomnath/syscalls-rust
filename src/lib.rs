19534f65e548dbe9a2ca1b12b88d6bb1915f1438
CodeOfSomnath <somnathnandi368@gmail.com>
2025-03-03 10:36:43 +0000

    adding docs and fixing code

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
+
+/// This module contains all the syscalls needed with their signatures and data types 
+/// This is not an implementation of syscalls in rust. This is just linking the available syscalls using ffi signatures
 pub mod syscalls;
+
+/// This module contains all the type we needed for a syscall functions work
+/// it will not conatain any extra types 
 pub mod types;
 
+/// This is a test module for this project
 mod test;
--- src/syscalls.rs
+++ src/syscalls.rs
@@ -1,4 +1,8 @@
+//// This module contains all the syscalls needed with their signatures and data types 
+//// This is not an implementation of syscalls in rust. This is just linking the available syscalls using ffi signatures
 
+
+/// This module supports all the 64 bit arch syscalls
 #[cfg(feature = "arch64")]
 pub mod arch64; // x86 64 bit, x86-64 abi
 
